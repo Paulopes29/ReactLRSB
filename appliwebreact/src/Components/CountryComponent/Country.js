0f7ac9d4eb6527b81ff0cd728de38b9799e8c967
Betamil <hugoribeyron1@gmail.com>
2022-03-30 16:54:24 +0200

    Recherche d'un département dynamiquement

--- appliwebreact/src/Components/CountryComponent/Country.js
+++ appliwebreact/src/Components/CountryComponent/Country.js
@@ -1,37 +1,44 @@
-import '../../App.css';
 import {useState} from "react";
-import {useEffect} from "react";
+import '../../App.css';
+import {Icon, NumericInput} from "@blueprintjs/core";
 
 function Country() {
     const [myCountry, setCountry] = useState([])
-    const [countryId] = useState(0)
 
-//   function fetchDataForCountryHandler() {
-//       fetch('http://api.zippopotam.us/FR/42400')
-//           .then(response => {
-//               console.log(response);
-//               return response.json();
-//           }).then(data => {
-//           console.log(data)
-//           setCountry(data.places);
-//       })
-//   }
-
-    const fetchDataForCountryHandler = async () =>  {
-        const httpResponse = await fetch("http://api.zippopotam.us/FR/42400");
+    const fetchDataForCountryHandler = async (apiAddress) =>  {
+        const httpResponse = await fetch(apiAddress);
         const httpData = await httpResponse.json();
         setCountry(httpData.places);
     }
 
-    useEffect(() => {
-        fetchDataForCountryHandler();
-    }, [countryId]);
+    const replacePostalCodeOnApiAddress = (postalCode) => {
+        const apiAddress = 'http://api.zippopotam.us/FR/42400'
+        console.warn(apiAddress);
+        const newApiAddress = apiAddress.replace(/\d{5}$/g, postalCode);
+        console.log(newApiAddress)
+        return newApiAddress;
+    }
+
+    const getValue = () => {
+        const value = document.getElementById('postalCodeInput').value;
+        console.warn(value)
+        const apiAddress = replacePostalCodeOnApiAddress(value)
+        fetchDataForCountryHandler(apiAddress);
+    }
 
     return (
         <div className="Country">
+            <NumericInput
+                id="postalCodeInput"
+                placeholder="Entrer un code postale"
+                leftIcon={<Icon icon="map-marker"/>}
+                buttonPosition="none">
+            </NumericInput>
+            <button type='submit' onClick={getValue}>Rechercher</button>
             {myCountry.length > 0 &&
                 myCountry.map((country) => {
-                return country.state })}
+                    console.warn(country)
+                    return country.state })}
         </div>
     );
 }
