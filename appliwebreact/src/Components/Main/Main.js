9d9806a6d29687073bd2ecae4b947833b55cbc6d
Hugo SILVESTRE <hugo.silvestre@outlook.fr>
2022-03-30 16:57:50 +0200

    update compo accueil

--- appliwebreact/src/Components/Main/Main.js
+++ appliwebreact/src/Components/Main/Main.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Route, Routes} from "react-router-dom";
-import Accueil from "../Accueil";
+import Accueil from "../Accueil/Accueil";
 import TestRoute from "../test";
 import LoginPage from "../Login/Login";
 
@@ -9,7 +9,7 @@ const Main = (props) => {
     return(
         <div>
             <Routes>
-                <Route path="/" element={<Accueil/>}/>*/}
+                <Route path="/" element={<Accueil/>}/>
                 <Route path="/Test" element={<TestRoute/>}/>
                 <Route path="/Login" element={<LoginPage/>}/>
             </Routes>
