f0166cfd31b2df7e8203561747d59546ddbdd792
Betamil <hugoribeyron1@gmail.com>
2022-03-30 17:22:30 +0200

    wip

--- appliwebreact/src/Components/Main/Main.js
+++ appliwebreact/src/Components/Main/Main.js
@@ -1,8 +1,9 @@
 import React from "react";
 import {Route, Routes} from "react-router-dom";
-import Accueil from "../Accueil";
+import Accueil from "../Accueil/Accueil";
 import TestRoute from "../test";
 import LoginPage from "../Login/Login";
+import Country from "../CountryComponent/Country";
 
 const Main = (props) => {
 
@@ -12,6 +13,7 @@ const Main = (props) => {
                 <Route path="/" element={<Accueil/>}/>*/}
                 <Route path="/Test" element={<TestRoute/>}/>
                 <Route path="/Login" element={<LoginPage/>}/>
+                <Route path="/Country" element={<Country/>}/>
             </Routes>
         </div>
     )
