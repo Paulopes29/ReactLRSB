b502cd6963669f29fd39efa8698dad8906368d0a
Betamil <hugoribeyron1@gmail.com>
2022-03-31 16:48:30 +0200

    Création d'une card pour récupérer les données retourner par l'API

--- appliwebreact/src/Components/Routes/Routes.js
+++ appliwebreact/src/Components/Routes/Routes.js
@@ -37,7 +37,7 @@ const RoutesSideBar = [
     {
         visible:true,
         path: "/Country",
-        name: "ville",
+        name: "Ville",
         icon: <FaList />,
         component: Country,
         layout: "",
